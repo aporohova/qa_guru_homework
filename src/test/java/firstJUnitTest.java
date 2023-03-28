4bbcbc003294549913bdd083d6e80f6e408e26b8
alena.porohova <alena.porohova@lamoda.ru>
2023-03-28 22:17:29 +0300

    add test example

--- src/test/java/firstJUnitTest.java
+++ src/test/java/firstJUnitTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class firstJUnitTest {
+    @Test
     void firstTest() {
         Assertions.assertTrue(3 > 2);
         System.out.println("        #### @Test firstTest()");
