159bcfffe8f55d39d666a213de801a746ae8d9c1
alena.porohova <alena.porohova@lamoda.ru>
2023-03-28 22:15:25 +0300

    change build gradle

---
+++ src/test/java/firstJUnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class firstJUnitTest {
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+        System.out.println("        #### @Test firstTest()");
+    }
+
+
+}
