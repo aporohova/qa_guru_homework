e7e8cd05ceb10af0868105352a6a5d5c66b9b73a
alena.porohova <alena.porohova@lamoda.ru>
2023-04-02 13:31:15 +0300

    add test (completed version)

--- src/test/java/com/demoqa/PracticeFormTests.java
+++ src/test/java/com/demoqa/PracticeFormTests.java
@@ -1,34 +1,54 @@
 package com.demoqa;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import java.io.File;
 
-import static com.codeborne.selenide.Condition.checked;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 public class PracticeFormTests extends TestBase {
 
     @Test
     void successfulFormTest (){
         open ("/automation-practice-form");
-        $("#firstName").setValue("Jack");
-        $("#lastName").setValue("London");
+        $("#firstName").setValue("James");
+        $("#lastName").setValue("Bond");
         $("#userEmail").setValue("JLondon@gggg.com");
         $("[type='radio'][value='Male']").parent().click();
         $("#userNumber").setValue("9995557771");
-        $("#dateOfBirthInput").selectOptionByValue("09 Feb 1932");
+        $("#dateOfBirthInput").sendKeys(Keys.COMMAND + "a"); //в случае Windows OS поменять на CONTROL
+        $("#dateOfBirthInput").sendKeys("12 Apr 1911");
+        $("#dateOfBirthInput").pressEnter();
+        $(".subjects-auto-complete__control").click();
+        $("#subjectsInput").sendKeys("C");
+        $("#react-select-2-option-2").click();
+        $("[type='checkbox'][value='3']").parent().click();
+        $("#uploadPicture").uploadFile(new File("src/test/data/kartinki_png_13_01075134-768x967-1.png"));
+        $("#currentAddress").setValue("Lenina street");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#state").click();
+        $(".css-11unzgr").$(byText("Rajasthan")).click();
+        $("#city").click();
+        $(".css-11unzgr").$(byText("Jaipur")).click();
+        $(".btn-primary").click();
+
+        //check the results
+        $(".table-responsive").shouldHave(text("James Bond"),
+                text("JLondon@gggg.com"),
+                text("Male"),
+                text("9995557771"),
+                text("12 April,1911"),
+                text("Computer science"),
+                text("Music"),
+                text("kartinki_png_13_01075134-768x967-1.png"),
+                text("Lenina street"),
+                text("Rajasthan Jaipur"));
+        sleep(20000);
 
 
-        //$("").selectRadio("");
-        //$("").uploadFile();
-        //$("").setValue("");
-        //$("").setValue(""); choose state
-        //$("").setValue(""); choose city
-        //$("").click();
 
-
-        $("").shouldHave(text(""));
 
 
     }
