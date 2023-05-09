18218dee1478702032ee8c14da8719e31786861f
alena.porohova <alena.porohova@lamoda.ru>
2023-05-09 12:45:35 +0300

    add remote Test

---
+++ src/test/java/com/demoqa/tests/PracticeFormRemoteTest.java
@@ -0,0 +1,45 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.demoqa.tests.TestData.*;
+
+public class PracticeFormRemoteTest extends RemoteTestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    @Tag("remote")
+    void successfulFormTest (){
+
+        registrationPage.openPage()
+                        .removeFooter()
+                        .setFirstName(firstName)
+                        .setLastName(lastName)
+                        .setUserEmail(userEmail)
+                        .setGender(gender)
+                        .setUserNumber(userNumber)
+                        .setBirthDate(day,month,year)
+                        .setSubject(subjects)
+                        .selectHobby(hobbies)
+                        .uploadUserPicture (pathValue, fileValue)
+                        .setCurrentAddress(currentAddress)
+                        .setState(states)
+                        .setCity(cities)
+                        .selectSubmit();
+        registrationPage.checkResults(firstName + " " + lastName)
+                        .checkResults( userEmail)
+                        .checkResults( gender)
+                        .checkResults( userNumber)
+                        .checkResults(day)
+                        .checkResults(month)
+                        .checkResults(year)
+                        .checkResults( subjects)
+                        .checkResults( hobbies)
+                        .checkResults(fileValue)
+                        .checkResults(currentAddress)
+                        .checkResults( states + " " + cities);
+    }
+}
+
