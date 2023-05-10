0ebe1d5cc4af33fadda3b98fea071cd750e2bf33
alena.porohova <alena.porohova@lamoda.ru>
2023-05-10 13:59:26 +0300

    add remote Test

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -1,7 +1,10 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class RemoteTestBase {
     @BeforeAll
@@ -10,5 +13,11 @@ public class RemoteTestBase {
         Configuration.browserSize = "1920x1080";
         //Configuration.browser = "chrome";
         Configuration.pageLoadStrategy = "eager";
+    }
+
+    @BeforeEach
+    void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
     }
 }
