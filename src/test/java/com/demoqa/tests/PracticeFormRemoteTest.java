8f530693a6a7b423ce0e2dde253fd874d3278a6b
alena.porohova <alena.porohova@lamoda.ru>
2023-05-14 15:08:58 +0300

    change properties

--- src/test/java/com/demoqa/tests/PracticeFormRemoteTest.java
+++ src/test/java/com/demoqa/tests/PracticeFormRemoteTest.java
@@ -12,7 +12,7 @@ public class PracticeFormRemoteTest extends RemoteTestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
-    @Tag("remote")
+    @Tag("remote") @Tag("blocker")
     @DisplayName("Successful registration")
     void successfulFormTest (){
 
